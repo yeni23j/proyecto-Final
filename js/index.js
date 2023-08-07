280d66195659aa6475706d2b9c480c7490419be2
Yeni <yeni_jerke@hotmail.com>
2023-08-07 19:37:27 -0300

    mediaq index

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const list = document.querySelector ('.list');
+
+const menu = document.querySelector ('.menu');
+
+list.addEventListener('click', () =>{
+    menu.classList.toggle('activado');
+});
